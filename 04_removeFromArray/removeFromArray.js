1c92c1f8013301f1ffdb23dd2e0f20d2a6b63f04
Stephanie Hadley <116458048+HadleyStephanie@users.noreply.github.com>
2022-12-24 11:38:51 -0800

    Exercises 1-7 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    // construct a new array from the argumaents passed in to the function and assign it
+	// to a new constant variable "args"
+	const args = Array.from(arguments);
+	
+	// take the first element of the constant array variable "args" and assign it to a new
+	// constant variable "originalArray"
+	const originalArray = args[0];
 
+	// take every element in the original parameter array "args" except for the first element
+	// and assign it to a new array variable "whatWeWantRemoved"
+	let whatWeWantRemoved = args.slice(1);
+	
+
+	// define a function called "checkNum" that takes in a single number in parameters
+	// and returns true if is not included in "whatWeWantRemoved" array, and false if it is included
+	function checkNum(currentArrayNum) {
+		return !whatWeWantRemoved.includes(currentArrayNum);
+    }
+
+	// filter the "originalArray" array variable using the "checkNum" function, removing
+	// all values in "originalArray" that are included in "whatWeWantRemoved", and
+	// assigning the resulting array to a new constant variable "newArray"
+	const newArray = originalArray.filter(checkNum);
+
+	// using big arrow notation to define the function to be passed into the filter
+	// instead of defining the function separately, like we did with "checkNum"
+	// const newArray = originalArray.filter(currentArrayNum => !whatWeWantRemoved.includes(currentArrayNum));
+
+	// return "newArray"
+	return newArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  //assigns fahrenheitTemp to the variable x
+  let x = fahrenheitTemp;
+  //assigns conversion of x to celcius to the varibale convertedCelTemp
+  let convertedCelTemp = Math.round((x - 32) * (5 / 9) * 10) / 10;
+  //returns the convertedCelTemp number that was calculated
+  return convertedCelTemp;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(celciusTemp) {
+  let x = celciusTemp;
+
+  let convertedFahrTemp = Math.round(((x * (9 / 5) + 32) * 10)) / 10;
+
+  return convertedFahrTemp;
 };
 
 // Do not edit below this line
